0bddec38ea032f04c22255f627445357fa26e21b
ahamathbasha <bashachristopherallah786@gmail.com>
2024-07-29 13:22:21 +0530

    datatypes in js

---
+++ basics/datatypes.js
@@ -0,0 +1,17 @@
+/*
+primitive datatype
+//number
+//bigint
+//string =>words or text
+//boolean=>true or false
+//null=>variable is not in the program and memory also.standalone value;
+//undefined=>variable is declared we have space in memory for the variable but we dont asssign the values for it.
+//symbol=>to find uniqueness
+*/
+
+/*non primitive datatypes*/
+//object
+
+//to find the datatype of the variable we use typeof
+let name="Hitesh";
+console.log(typeof name);
