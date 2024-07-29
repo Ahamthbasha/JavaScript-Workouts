17d84bf2d8cbdc2bc87c25de4f7ca39d1fceb55d
ahamathbasha <bashachristopherallah786@gmail.com>
2024-07-29 16:33:14 +0530

    CONVERSIONS

---
+++ basics/conversionoperations.js
@@ -0,0 +1,23 @@
+let score="33abc";
+console.log(typeof score);
+console.log(score)
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber); 
+//for string=Nan;
+//for null=0;
+//for undefined= nan;
+//for boolean true=1 (or) false=0 (or) empty=zero;
+//number to boolean
+let number=1;
+let boolean=Boolean(number);
+console.log(boolean);
+//let number2=""
+let number2=0;
+let boolean2=Boolean(number2);
+console.log(boolean2);
+//number to string
+let n=1;
+let stringn=String(n);
+console.log(stringn);
+console.log(typeof stringn);
