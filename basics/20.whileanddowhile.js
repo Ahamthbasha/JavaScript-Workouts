6ae7426a136563222fb7f9d85cb27b503a7bb533
ahamathbasha <bashachristopherallah786@gmail.com>
2024-07-31 10:57:44 +0530

    while dowhile loop

---
+++ basics/20.whileanddowhile.js
@@ -0,0 +1,20 @@
+//while
+let i=1
+while(i<=10){
+    console.log(i)
+    i++
+}
+//printing array by while loop
+let marks=[99,98,100,70,80]
+let j=0
+while(j<marks.length){
+    console.log(marks[j]);
+    j++
+}
+//do while
+let score=13
+do{
+    console.log(score)
+    score++
+}
+while(score<=12)
