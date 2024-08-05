4d5ef9b02bc520c8d52dda00c0f3238f6176fd31
ahamathbasha <bashachristopherallah786@gmail.com>
2024-08-05 10:40:28 +0530

    objectdestructuring

---
+++ week2js/objectdestructuring.js
@@ -0,0 +1,27 @@
+const person={
+    name:"abin",
+    age:20,
+    address:{
+    city:"chennai",
+    street:"thuraipaakkam",
+    }
+};
+/*const {name:fullname,age:Age}=person;
+console.log(fullname);
+console.log(Age);
+const {name,age, address: {city,street}}=person;
+console.log(person.address.city)*/
+//destructuring in function parameters
+function display({name,age}){
+    console.log(`Name:${name}`)
+    console.log(`Age:${age}`)
+}
+const workers={
+    name:"basha",
+    age:20
+};
+display(workers)
+const colors=['red','green','yellow']
+const [firstColor ,secondColor]=colors;
+console.log(firstColor)
+console.log(secondColor)
