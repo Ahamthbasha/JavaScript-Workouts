b67f4fcc72b59a35bc710ef52ed4da9fd54e39d1
ahamathbasha <bashachristopherallah786@gmail.com>
2024-08-05 21:38:51 +0530

    datetime

---
+++ week2js/datetime.js
@@ -0,0 +1,14 @@
+const date=new Date();
+console.log(date)
+/*
+console.log(date.getFullYear())
+console.log(date.getMonth())
+console.log(date.getDate())
+console.log(date.getTime())
+console.log(date.getSeconds())
+console.log(date.getMinutes())
+console.log(date.getMonth())*/
+date.setFullYear(2025)
+date.setMonth(11)
+date.setDate(25)
+console.log(date)
