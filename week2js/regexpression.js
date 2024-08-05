932b768731737cb65fcb195144a2d3e047e0e7ae
ahamathbasha <bashachristopherallah786@gmail.com>
2024-08-05 18:02:06 +0530

    regexpression

---
+++ week2js/regexpression.js
@@ -0,0 +1,36 @@
+/*const Name='hello world';
+const regex=/hello.world/s;
+const match=Name.match(regex);
+console.log(match);
+let text="red , green ,orange";
+let n=text.match(/(red|green)/g);
+console.log(n)
+//d which is usefull in matching with numbers
+let regEx=/\d+/g;
+let text1="tomato is 30 rupees"
+console.log(text1.match(regEx))
+//s which is usefull in splitting or whitespaces
+let regEx1=/\s+/g;
+let text2="tomato is 30 rupees"
+console.log(text2.split(regEx1))
+//b which is useful to match at the beginning of a word or end of the word
+let regEx3=/\bword/;
+let text3="world speak word are fine";
+console.log(text3.search(regEx3))
+console.log(text3.match(regEx3))
+console.log(regEx3.test(text3))
+let regEx4=/o+/g;
+let text4="ooo"
+console.log(text4.match(regEx4))
+console.log(text4.search(regEx4))
+console.log(regEx4.test(text4))
+let regEx=/a+/g;
+let text="global gas issues"
+console.log(text.match(regEx))
+/*let regEx6=/a*/
+/*let text="abcdefghiln"
+console.log(text.search(regEx6))
+console.log(regEx6.exec(text))
+let regEx7=/a?/g;
+let text="batman"
+console.log(regEx7.exec(text))*/
