02cf5a64eabcc5954d6fe1db0e2cc6e4a8bedcc0
ahamathbasha <bashachristopherallah786@gmail.com>
2024-08-05 12:27:25 +0530

    variables

---
+++ week2js/variable.js
@@ -0,0 +1,20 @@
+/*//console.log(a)
+//console.log(b)
+var a=10
+let b=20
+console.log(a)
+console.log(b)*
+function n(){
+    //console.log(a)console.log(b)
+    let a=10;
+    let b=20;
+    console.log(a)
+    console.log(b)
+}
+n()*/
+//undefined
+let a;
+console.log(a);
+let b=null;
+console.log(b)
+console.log(y)//reference error not defined
