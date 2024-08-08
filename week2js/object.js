381df80d87df66c8af04895b32fba33eac4cf190
ahamathbasha <bashachristopherallah786@gmail.com>
2024-08-08 10:43:21 +0530

    object create

---
+++ week2js/object.js
@@ -0,0 +1,22 @@
+const person={
+    name:"ahamathbasha",
+    role:"mern developer",
+    age:30,
+    greet:function(){
+        return `hi ${this.name} you are a ${this.role}`
+    }
+}
+console.log(person.name)
+console.log(person["role"])
+console.log(person.greet())
+//es6 
+const movie={
+    name:"munna bhai mbbs",
+    director:"rajkumar hirani",
+    greet(){
+    return `my favourite movie of all time is ${this.name} which was directed by ${this.director}`
+    }
+}
+console.log(movie.name)
+console.log(movie.director)
+console.log(movie.greet())
