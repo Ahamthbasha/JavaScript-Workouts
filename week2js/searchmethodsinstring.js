03e2aaa36358b61dc60cb70957f30d5bf0e68a9f
ahamathbasha <bashachristopherallah786@gmail.com>
2024-08-09 18:54:52 +0530

    search

---
+++ week2js/searchmethodsinstring.js
@@ -0,0 +1,17 @@
+const name="salaman paapaiya"
+console.log(name.indexOf('a'))
+console.log(name.lastIndexOf('a'))
+const lastName="paapaiya"
+console.log(name.search(lastName))
+console.log(name.match(lastName))
+console.log(name.includes(lastName))
+console.log(name.startsWith(lastName))
+console.log(name.endsWith(lastName))
+let n=3
+n=String(3)
+console.log(typeof n)
+let n1=1234.89898
+console.log(n1.toFixed(2))
+console.log(n1.toPrecision(4))
+console.log(n1.toExponential(4))
+console.log(n1.valueOf())
